f7b582d0ddfdb48259d10d3bbb8662e023fb812e
scottkang123 <scottkang991103@gmail.com>
2025-07-14 04:21:10 +0000

    logger problem 1

--- homework2/homework/logger.py
+++ homework2/homework/logger.py
@@ -30,11 +30,17 @@ def test_logging(logger: tb.SummaryWriter):
             dummy_train_accuracy = epoch / 10.0 + torch.randn(10)
 
             # TODO: log train_loss
+            logger.add_scalar('train_loss', dummy_train_loss, global_step)
+
             # TODO: save additional metrics to be averaged
+            metrics["train_acc"].append(dummy_train_accuracy)
+
 
             global_step += 1
 
         # TODO: log average train_accuracy
+        avg_train_acc = torch.cat(metrics["train_acc"]).mean()
+        logger.add_scalar('train_accuracy', avg_train_acc, global_step)
 
         # example validation loop
         torch.manual_seed(epoch)
@@ -42,8 +48,12 @@ def test_logging(logger: tb.SummaryWriter):
             dummy_validation_accuracy = epoch / 10.0 + torch.randn(10)
 
             # TODO: save additional metrics to be averaged
+            metrics["val_acc"].append(dummy_validation_accuracy)
+
 
         # TODO: log average val_accuracy
+        avg_val_acc = torch.cat(metrics["val_acc"]).mean()
+        logger.add_scalar('val_accuracy', avg_val_acc, global_step)
 
 
 if __name__ == "__main__":
